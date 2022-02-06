a922cb19d10c5828129683fd419d11ee42f90828
Honey Bhasvar <52161860+honey-bhavsar@users.noreply.github.com>
2022-02-06 17:36:59 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,14 @@
+import pyttsx3
+import PyPDF2
+
+book = open('acok.pdf', 'rb')
+pdfReader = PyPDF2.PdfFileReader(book)
+pages = pdfReader.numPages
+print(pages)
+speaker = pyttsx3.init()
+for num in range(1, pages):
+    page = pdfReader.getPage(1)
+    text = page.extractText()
+    speaker.say(text)
+    print(text)
+    speaker.runAndWait()
